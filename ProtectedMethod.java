19bdb10c5f30919f7711364e7782eca27db0db4a
karituason <karituason@users.noreply.github.com>
2019-08-11 17:55:41 -0400

    Add files via upload

---
+++ ProtectedMethod.java
@@ -0,0 +1,7 @@
+package assignment1.protect;
+
+public class ProtectedMethod {
+	protected void protectMethod() {
+		System.out.println("This is a protected method in package \'assignment1.protect\'.");
+	}
+}
